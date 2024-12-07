26075a8aeb010d15788d336af1dcbb132c21e31d
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-12-07 21:00:32 +0200

    Problem solved. :)

---
+++ LeetCode/1004_MaxConsecutiveOnes3/MaxConsecutiveOnes3.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode._1004_MaxConsecutiveOnes3
+{
+    public class MaxConsecutiveOnes3
+    {
+        public int Solve(int[] nums, int k)
+        {
+            int left = 0;
+            int right = 0;
+            int currentZeroCount = 0;
+
+            while (right < nums.Length)
+            {
+                if (nums[right] == 0)
+                {
+                    currentZeroCount++;
+                }
+
+                right++;
+
+                if (currentZeroCount > k)
+                {
+                    if (nums[left] == 0)
+                    {
+                        currentZeroCount--;
+                    }
+                    left++;
+                }
+            }
+
+            return right - left;
+        }
+    }
+}
