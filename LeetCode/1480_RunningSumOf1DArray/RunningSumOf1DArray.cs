71138f0887a6c696201acbbe8109fed4e9242946
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-12-08 16:56:34 +0200

    Problem solved.

---
+++ LeetCode/1480_RunningSumOf1DArray/RunningSumOf1DArray.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode._1480_RunningSumOf1DArray
+{
+    public class RunningSumOf1DArray
+    {
+        public int[] Solve(int[] nums)
+        {
+            int numsLength = nums.Length;
+
+            int[] result = new int[numsLength];
+
+            result[0] = nums[0];
+
+            for (int i = 0; i < numsLength; i++)
+            {
+                result[i] = result[i - 1] + nums[i];
+            }
+
+            return result;
+        }
+    }
+}
