2bcb51c5abb13c23e614fe0e823ed6445df089b1
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-12-08 14:40:50 +0200

    Problem solved.

---
+++ LeetCode/2270_NumberOfWaysToSplitAnArray/NumberOfWaysToSplitAnArray.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode._2270_NumberOfWaysToSplitAnArray
+{
+    public class NumberOfWaysToSplitAnArray
+    {
+        public int Solve(int[] nums)
+        {
+            if (nums.Length < 2)
+                return 0;
+
+            /* First intuitive approach */
+
+            //int numsLength = nums.Length;
+
+            //long[] prefix = new long[numsLength];
+
+            //prefix[0] = nums[0];
+
+            //for (int i = 1; i < numsLength; i++)
+            //{
+            //    prefix[i] = prefix[i - 1] + nums[i];
+            //}
+
+            //int result = 0;
+
+            //for (int i = 0; i < numsLength - 1; i++)
+            //{
+            //    long leftSection = prefix[i];
+
+            //    if (leftSection >= prefix[numsLength - 1] - leftSection)
+            //    {
+            //        result++;
+            //    }
+            //}
+
+            //return result;
+
+            /* Faster appraoch without prefix array */
+
+            long sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum = sum + nums[i];
+            }
+
+            int result = 0;
+            long leftSection = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                leftSection += nums[i];
+                long rightSection = sum - leftSection;
+
+                if (leftSection >= rightSection)
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
