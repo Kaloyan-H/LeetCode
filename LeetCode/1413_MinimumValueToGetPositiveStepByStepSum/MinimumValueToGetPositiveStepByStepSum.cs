8249746237ac2456cc55bca6a39298bfed3819ff
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-12-08 17:20:33 +0200

    Problem solved.

---
+++ LeetCode/1413_MinimumValueToGetPositiveStepByStepSum/MinimumValueToGetPositiveStepByStepSum.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._1413_MinimumValueToGetPositiveStepByStepSum
+{
+    public class MinimumValueToGetPositiveStepByStepSum
+    {
+        public int Solve(int[] nums)
+        {
+            int prefixSum = 0;
+            int minimumPrefixSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                prefixSum += nums[i];
+
+                if (prefixSum < minimumPrefixSum)
+                {
+                    minimumPrefixSum = prefixSum;
+                }
+            }
+
+            return -minimumPrefixSum + 1;
+        }
+    }
+}
