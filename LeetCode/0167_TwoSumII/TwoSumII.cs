7f8508dda883ed3654f9be06d63efc516248db31
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2026-01-27 15:42:54 +0200

    feat: solved 167.TwoSumII

---
+++ LeetCode/0167_TwoSumII/TwoSumII.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._0167_TwoSumII
+{
+    public class TwoSumII
+    {
+        public static int[] Solve(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int sum = nums[left] + nums[right];
+
+                if (sum == target)
+                {
+                    return new int[] { left + 1, right + 1 };
+                }
+                else if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+    }
+}
+
