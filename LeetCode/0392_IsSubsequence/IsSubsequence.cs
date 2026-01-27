994bcf51f5a83dd7064cc1d52430ffecbec702e8
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2026-01-27 16:08:36 +0200

    feat: solved 392.IsSubsequence

---
+++ LeetCode/0392_IsSubsequence/IsSubsequence.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode._0392_IsSubsequence
+{
+    public class IsSubsquence
+    {
+        public bool Solve(string s, string t)
+        {
+            if (s.Length == 0)
+            {
+                return true;
+            }
+
+            int i = 0;
+            int j = 0;
+
+            while (i < s.Length && j < t.Length)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                }
+
+                j++;
+            }
+
+            return i == s.Length;
+        }
+    }
+}
