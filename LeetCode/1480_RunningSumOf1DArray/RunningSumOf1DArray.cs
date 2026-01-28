f38f351ed60ff8480b1e3ad78325bdddfa6fc6ec
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2026-01-28 15:28:29 +0200

    fix: fixed solution

--- LeetCode/1480_RunningSumOf1DArray/RunningSumOf1DArray.cs
+++ LeetCode/1480_RunningSumOf1DArray/RunningSumOf1DArray.cs
@@ -10,7 +10,7 @@
 
             result[0] = nums[0];
 
-            for (int i = 0; i < numsLength; i++)
+            for (int i = 1; i < numsLength; i++)
             {
                 result[i] = result[i - 1] + nums[i];
             }
