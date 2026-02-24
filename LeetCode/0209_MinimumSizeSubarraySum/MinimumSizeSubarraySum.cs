9f6bd2fb3d4b525f246f4d11b1cf80b772ee3587
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2026-02-24 19:51:43 +0200

    solved a few more and fixed an old solution

---
+++ LeetCode/0209_MinimumSizeSubarraySum/MinimumSizeSubarraySum.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode._0209_MinimumSizeSubarraySum
+{
+    public class MinimumSizeSubarraySum
+    {
+	    public int Solve(int target, int[] nums)
+        {
+            int start = 0;
+            int end = 0;
+            int sum = 0;
+            int minLen = int.MaxValue;
+
+            while (end < nums.Length)
+            {
+                sum += nums[end];
+
+                while (sum >= target)
+                {
+                    minLen = end - start + 1 < minLen ? end - start + 1 : minLen;
+                    sum -= nums[start];
+                    start++;
+                }
+
+                end++;
+            }
+
+            return minLen == int.MaxValue ? 0 : minLen;
+        }
+	}
+}
---
+++ LeetCode/1456_MaximumNumberOfVowelsInASubstringOfGivenLength/MaximumNumberOfVowelsInASubstringOfGivenLength.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode._1456_MaximumNumberOfVowelsInASubstringOfGivenLength
+{
+
+    public class MaximumNumberOfVowelsInASubstringOfGivenLength
+    {
+        public int Solve(string s, int k)
+        {
+            int start = 0;
+            int end = 0 + k - 1;
+            int counter = 0;
+
+            for (int i = 0; i <= end; i++)
+            {
+                char current = s[i];
+
+                if (current == 'a' || current == 'e' || current == 'i' || current == 'o' || current == 'u')
+                    counter++;
+            }
+
+            int maxNumber = counter;
+
+            while (end < s.Length - 1)
+            {
+                char oldest = s[start];
+                char newest = s[end + 1];
+
+                if (oldest == 'a' || oldest == 'e' || oldest == 'i' || oldest == 'o' || oldest == 'u')
+                    counter--;
+                if (newest == 'a' || newest == 'e' || newest == 'i' || newest == 'o' || newest == 'u')
+                    counter++;
+
+                end++;
+                start++;
+
+                if (counter > maxNumber)
+                    maxNumber = counter;
+            }
+
+            return maxNumber;
+        }
+	}
+}
