a7472203ac67d400a8c4807a42eb698388f5be38
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2026-03-20 23:44:42 +0200

    more solved

---
+++ LeetCode/0525_ContiguousArray/ContiguousArray.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode._0525_ContiguousArray
+{
+    public class ContiguousArray
+    {
+        public int Solve(int[] nums)
+        {
+            Dictionary<int, int> firstDiffAppearance = new Dictionary<int, int>();
+
+            firstDiffAppearance[0] = -1;
+
+            int zeros = 0;
+            int ones = 0;
+            int maxSubarrLength = 0;
+
+            int numsLength = nums.Length;
+
+            for (int i = 0; i < numsLength; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    zeros++;
+                }
+                else
+                {
+                    ones++;
+                }
+
+                int diff = zeros - ones;
+
+                if (firstDiffAppearance.ContainsKey(diff))
+                {
+                    maxSubarrLength = Math.Max(maxSubarrLength, i - firstDiffAppearance[diff]);
+                }
+                else
+                {
+                    firstDiffAppearance.Add(diff, i);
+                }
+            }
+
+            return maxSubarrLength;
+        }
+    }
+}
