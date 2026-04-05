6f04856aa97103a1275198381c71f88b9a899024
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2026-04-05 23:02:43 +0300

    new solution for an old problem

--- LeetCode/0239_SlidingWindowMaximum/SlidingWindowMaximum.cs
+++ LeetCode/0239_SlidingWindowMaximum/SlidingWindowMaximum.cs
@@ -4,61 +4,116 @@
     {
         public static int[] Solve(int[] nums, int k)
         {
-            if (k == 1)
-            {
-                return nums;
-            }
+            // ------------------------
+            // OPTIMIZED DEQUE APPROACH
+            // ------------------------
 
-            int counter = k;
-            int maximumTemp = int.MinValue;
+            LinkedList<int> deque = new LinkedList<int>();
 
-            for (int right = 0; right < k - 1; right++)
+            for (int i = 0; i < k; i++)
             {
-                if (nums[right] > maximumTemp)
+                int current = nums[i];
+
+                while (deque.Count > 0 && nums[deque.Last!.Value] < current)
                 {
-                    maximumTemp = nums[right];
-                    counter = k;
+                    deque.RemoveLast();
                 }
 
-                counter--;
+                deque.AddLast(i);
             }
 
-            int[] result = new int[nums.Length - k + 1];
+            int length = nums.Length;
+            int[] answer = new int[length - k + 1];
 
-            for (int right = k - 1; right < nums.Length; right++)
+            int right = k - 1;
+
+            for (int left = 0; left < length - k + 1; left++)
             {
-                if (nums[right] > maximumTemp)
-                {
-                    maximumTemp = nums[right];
-                    result[right - k + 1] = nums[right];
-                    counter = k - 1;
-                }
-                else if (counter == 0)
-                {
-                    maximumTemp = int.MinValue;
+                int currentLargestIndex = deque.First!.Value;
 
-                    for (int innerRight = right - k + 1; innerRight <= right; innerRight++)
+                answer[left] = nums[currentLargestIndex];
+
+                if (currentLargestIndex == left)
+                {
+                    deque.RemoveFirst();
+                }
+
+                right++;
+
+                if (right < length)
+                {
+                    int newRight = nums[right];
+
+                    while (deque.Count > 0 && nums[deque.Last!.Value] < newRight)
                     {
-                        if (nums[innerRight] > maximumTemp)
-                        {
-                            maximumTemp = nums[innerRight];
-                            counter = k;
-                        }
-
-                        counter--;
+                        deque.RemoveLast();
                     }
 
-                    result[right - k + 1] = maximumTemp;
-                }
-                else
-                {
-                    result[right - k + 1] = maximumTemp;
-
-                    counter--;
+                    deque.AddLast(right);
                 }
             }
 
-            return result;
+            return answer;
+
+            // -------------------------------------------------------------------
+            // INITIAL SOLUTION FROM A LONG TIME AGO, NO DSA KNOWLEDGE AT THE TIME
+            // -------------------------------------------------------------------
+
+            //if (k == 1)
+            //{
+            //    return nums;
+            //}
+
+            //int counter = k;
+            //int maximumTemp = int.MinValue;
+
+            //for (int right = 0; right < k - 1; right++)
+            //{
+            //    if (nums[right] > maximumTemp)
+            //    {
+            //        maximumTemp = nums[right];
+            //        counter = k;
+            //    }
+
+            //    counter--;
+            //}
+
+            //int[] result = new int[nums.Length - k + 1];
+
+            //for (int right = k - 1; right < nums.Length; right++)
+            //{
+            //    if (nums[right] > maximumTemp)
+            //    {
+            //        maximumTemp = nums[right];
+            //        result[right - k + 1] = nums[right];
+            //        counter = k - 1;
+            //    }
+            //    else if (counter == 0)
+            //    {
+            //        maximumTemp = int.MinValue;
+
+            //        for (int innerRight = right - k + 1; innerRight <= right; innerRight++)
+            //        {
+            //            if (nums[innerRight] > maximumTemp)
+            //            {
+            //                maximumTemp = nums[innerRight];
+            //                counter = k;
+            //            }
+
+            //            counter--;
+            //        }
+
+            //        result[right - k + 1] = maximumTemp;
+            //    }
+            //    else
+            //    {
+            //        result[right - k + 1] = maximumTemp;
+
+            //        counter--;
+            //    }
+            //}
+
+            //return result;
         }
     }
 }
